d3592307f9f5a7d87982ed829aa53f4b2dabb6a4
Andikariski <andikariski207@gmail.com>
2020-12-21 16:59:09 +0700

    test

---
+++ application/views/pendaftar/bantuan_carauploadfile.php
@@ -0,0 +1,25 @@
+<div class="container-fluid">
+
+    <div class="row p-3">
+        <!-- DataTales Example -->
+        <div class="col">
+            <div class="card shadow-sm mb-4">
+                <div class="card-header py-3">
+                    <h6 class="mt-2 font-weight-bold text-primary">Panduan upload berkas</h6>
+                </div>
+                <div class="card-body">
+                    <h6 class="p-1">1. Siapkan berkas yang di butuhkan</h6>
+                    <h6 class="p-1">2. Foto siswa berupa image dengan format JPG,JPEG,PNG</h6>
+                    <h6 class="p-1">3. Scan rapot siswa kelas 6, dalam bentuk file PDF</h6>
+                    <h6 class="p-1">4. Scan bukti prestasi, jika ada banyak boleh jadikan 1 file PDF</h6>
+                    <h6 class="p-1">5. Scan kartu keluarga, format file JPEG,PNG,JPG dan PDF</h6>
+                    <h6 class="p-1">5. Scan akta kelahiran, format file JPEG,PNG,JPG dan PDF</h6>
+                    <div class="float-right">
+                        <a class="btn btn-danger btn-sm" href="<?= base_url('pendaftar') ?>">Kembali</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
---
+++ application/views/pendaftar/bantuan_isiform.php
@@ -0,0 +1,26 @@
+<div class="container-fluid">
+
+    <div class="row p-3">
+        <!-- DataTales Example -->
+        <div class="col">
+            <div class="card shadow-sm mb-4">
+                <div class="card-header py-3">
+                    <h6 class="mt-2 font-weight-bold text-primary">Panduan pengisian formulir</h6>
+                </div>
+                <div class="card-body">
+                    <h6 class="p-1">1. Isi data siswa terlebih dahulu</h6>
+                    <h6 class="p-1">2. Setelah mengisi data siswa kemudian mengisi data orang tua</h6>
+                    <h6 class="p-1">3. Kemudain setelah mengisi data orang tua, lanjut mengisi data wali</h6>
+                    <h6 class="p-1">4. Setelah data semua telah di isi dengan teliti, lanjut pada upload berkas pendaftaran</h6>
+                    <h6 class="p-1">5. Setelah berkas terpilih klik button kirim pendaftaran</h6>
+                    <h6 class="p-1">6. Data pendaftaran berhasil terkirim </h6>
+                    <h6 class="p-1">7. Perikas email , kode pendaftaran di kirim ke email yang telah di cantumkan saat pengisian formulir</h6>
+                    <div class="float-right">
+                        <a class="btn btn-danger btn-sm" href="<?= base_url('pendaftar') ?>">Kembali</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
